0d8e3b677df073b23cef6fca395fae440bdb4c16
Adam Davis <a.davis.dev@gmail.com>
2025-01-30 20:43:12 -0500

    feat(app): add some global styles

---
+++ src/lib/styles.ts
@@ -0,0 +1,6 @@
+import clsx from "clsx";
+
+export const style = {
+    linkStyle: clsx('text-purple-400', 'hover:underline'),
+    actionLink: clsx('text-purple-400', 'hover:underline', 'cursor-pointer'),
+}
